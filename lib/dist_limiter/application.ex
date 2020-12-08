277c726b9bd5b7b08432a8648e2d6affb9b7e6ef
Jechol Lee <mr.jechol@gmail.com>
2020-12-09 01:58:05 +0900

    trigger actions

--- lib/dist_limiter/application.ex
+++ lib/dist_limiter/application.ex
@@ -3,7 +3,6 @@ defmodule DistLimiter.Application do
 
   def start(_type, _args) do
     children = [
-      # The Stack is a child started via Stack.start_link([:hello])
       %{
         id: UniPg,
         start: {UniPg, :start_link, [DistLimiter]}
